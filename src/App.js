53c4bf70e81ca807ba0a26d302a5cf56825aaefa
TashtandyevBaysal <nurlanibakov1@gmail.com>
2024-08-29 13:34:24 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from "./widgets/Header/ui/header";
+import Home from "./pages/Home/Home";
+import Pokedex from './pages/Pokedex/Pokedex';
+import VideoGamesApps from "./pages/VideoGamesApps/PlayPokemonEvents";
+import Animation from './pages/Animation/Animation';
+import News from './pages/News/News';
+import TradingCardGame from './pages/TradingCardGame/TradingCardGame';
+import PlayPokemonEvents from './pages/PlayPokemonEvents/PlayPokemonEvents';
 import cls from "./App.module.scss"
 
-// i'ts basik branch
+// It's basic branch
+const App = () => {
+    return (
+        <div className={cls.App}>
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/Pokedex" element={<Pokedex/>}/>
+                    <Route path="/VideoGamesApps" element={<VideoGamesApps/>}/>
+                    <Route path="/Animation" element={<Animation/>}/>
+                    <Route path="/News" element={<News/>}/>
+                    <Route path="/TradingCardGame" element={<TradingCardGame/>}/>
+                    <Route path="/PlayPokemonEvents" element={<PlayPokemonEvents/>}/>
+                </Routes>
+            </Router>
+        </div>
 
-function App() {
-  return (
-    <div className={cls.App}>
-
-    </div>
-  );
-}
+    );
+};
 
 export default App;
---
+++ src/pages/TradingCardGame/TradingCardGame.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TradingCardGame = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TradingCardGame;
